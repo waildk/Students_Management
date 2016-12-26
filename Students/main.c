2fc58b92e6c3a0bf96e238730d72d74828b53099
waildk <wailallaoui1997@gmail.com>
2016-12-27 00:10:49 +0100

    Update main.c

--- Students/main.c
+++ Students/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "TPfinal.h"
+#include "headers/TPfinal.h"
+#include "TPfinal.c"
 
 int main()
 {
     int s  ;
     listeEtud *promo = NULL ;
     promo = (listeEtud *) malloc(sizeof(*promo)); /// Allouer l'espace necessaire pour la liste des etudiants
-    promo->first = NULL ;                  /// initaliser la liste à NULL
+    promo->first = NULL ;                  /// initaliser la liste Ã  NULL
     promo->taille = 0 ;                    /// initaliser la taille de la liste
         do{
-            s = Menu(promo);               /// repèter l'affichage du menu jusqu'a taper 0 ( exit )
+            s = Menu(promo);               /// repÃ¨ter l'affichage du menu jusqu'a taper 0 ( exit )
         }while ( s != 0 );
 return 0;
 }
